f8c6cdb0a032314cb7b4a5a863bd56306d173059
Jonathan García <jona03g97@gmail.com>
2021-05-23 19:47:53 -0600

    [Funcionamiento] Intentando enviar datos

---
+++ utils/clientes_agregar.js
@@ -0,0 +1,41 @@
+const $form = document.getElementById("add__form");
+const $form_btn = document.getElementById("add__button");
+
+const sendContact = async () => {
+  const formData = new FormData($form);
+  try {
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName: formData.get("firstName"),
+        secondName: formData.get("secondName"),
+        firstLastname: formData.get("firstLastname"),
+        secondLastname: formData.get("secondLastname"),
+        email: formData.get("email"),
+        prefix: formData.get("prefix"),
+        phone: formData.get("phone"),
+        cellphone: formData.get("cellphone"),
+        extension: formData.get("extension"),
+        tags: formData.get("tags"),
+      }),
+    };
+
+    const response = await fetch(
+      "https://portal-cesa.vercel.app/api/contact",
+      fetchOptions
+    );
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+$form_btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  sendContact();
+});
