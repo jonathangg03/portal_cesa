20ed69fe08ad18344e8d1c1e1d8734687ab9238c
Jonathan García <jona03g97@gmail.com>
2021-07-15 01:34:02 -0600

    Añadiendo variables de entorno

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
+
 module.exports = {
   api: {
     host: process.env.API_HOST || "localhost",
     port: process.env.API_PORT || 3000,
   },
   mysql: {
-    host: process.env.MYSQL_HOST || "remotemysql.com",
-    user: process.env.MYSQL_USERNAME || "9rr876KlLn",
-    password: process.env.MYSQL_PASSWORD || "tCE8wcd5qR",
-    database: process.env.MYSQL_DATABASE || "9rr876KlLn",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   },
-  secret: process.env.SECRET || "showSomeR3sp3ct",
+  secret: process.env.SECRET,
 };
